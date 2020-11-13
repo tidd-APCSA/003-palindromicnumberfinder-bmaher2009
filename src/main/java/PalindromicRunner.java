a42e648f7861c59e81ca6fff778d1262892aa690
Brendan Maher <brendangmaher@gmail.com>
2020-11-13 22:10:13 +0000

    submitted

--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,7 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
-
+        PalindromicNumberFinder numFind = new PalindromicNumberFinder(34534512);
+        numFind.searchForPalindromicNum(34534512);
     }
 }
